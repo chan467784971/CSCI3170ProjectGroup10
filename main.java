51df0218d69a226be508bcfc9c893b70baefc213
yuri <yurichan@YurideMacBook-Air.local>
2022-03-28 17:31:38 +0800

    second time java create

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class HelloWorld
+{
+    public static void main(String argv[])
+    {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        System.out.println(input);
+    }
+}
