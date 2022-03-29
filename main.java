eb2a64326dd73f02c22bb8bd30501c03e10bbe0b
yuri <yurichan@YurideMacBook-Air.local>
2022-03-29 12:43:48 +0800

    Menu add

--- main.java
+++ main.java
@@ -4,8 +4,32 @@ class Helloworld
 {
     public static void main(String args[])
     {
+        System.out.println("Welcome to Car Renting System!\n");
+        System.out.println("\n");
+        System.out.println("-----Main menu----\n");
+        System.out.println("What kinds of operations would you like to perform?\n");
+        System.out.println("1. Operations for Administrator\n");
+        System.out.println("2. Operations for User\n");
+        System.out.println("3. Operations for Manager\n");
+        System.out.println("4. Exit this program\n");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        System.out.println(input);
+
+        switch (input){
+            case "1" :
+            System.out.println("You choose Admin\n");
+            break;
+            case "2" :
+            System.out.println("You choose User\n");
+            break;
+            case "3" :
+            System.out.println("You choose Manager\n");
+            break;
+            case "4" :
+            System.out.println("You wanna leave\n");
+            default :
+            System.out.println("WDYM\n");
+            
+        }
     }
 }
