9c58fc040353d7c627d961bf9da30d2a0a67701e
LamTszHoi <100935287+LamTszHoi@users.noreply.github.com>
2022-04-16 21:37:09 +0800

    Update Database.java
    car: copyNum -> copyCount
    add user operation

--- model/Database.java
+++ model/Database.java
@@ -33,7 +33,7 @@ public class Database {
         // preparedstatement ???????
         PreparedStatement[] statements = {
             //SQL create table statement
-            connect.prepareStatement("CREATE TABLE car (callNum CHAR(8) NOT NULL, copyNum INTEGER(1) NOT NULL, carName CHAR(10) NOT NULL, company CHAR(25) NOT NULL, manufacture DATE NOT NULL, timeRent INTEGER(2) NOT NULL, ccId INTEGER(1) NOT NULL, PRIMARY KEY (callNum))"),
+            connect.prepareStatement("CREATE TABLE car (callNum CHAR(8) NOT NULL, copyCount INTEGER(1) NOT NULL, carName CHAR(10) NOT NULL, company CHAR(25) NOT NULL, manufacture DATE NOT NULL, timeRent INTEGER(2) NOT NULL, ccId INTEGER(1) NOT NULL, PRIMARY KEY (callNum))"),
             connect.prepareStatement("CREATE TABLE carCategory (ccId INTEGER(1) NOT NULL, ccName CHAR(20) NOT NULL, PRIMARY KEY (ccId))"),
             connect.prepareStatement("CREATE TABLE user (uId CHAR(12) NOT NULL, name CHAR(25) NOT NULL, age INTEGER(2) NOT NULL, occupation CHAR(20) NOT NULL, ucId INTEGER(1) NOT NULL, PRIMARY KEY (uId))"),
             connect.prepareStatement("CREATE TABLE userCategory (ucId INTEGER(1) NOT NULL, maxNum INTEGER(1) NOT NULL, period INTEGER(2) NOT NULL, PRIMARY KEY(ucId))"),
@@ -95,7 +95,103 @@ public class Database {
     }
 
     //Admin operation end
+    
+    
+    //User operation
+    public void listCarByCallNum(String CallNum){
+        try{
+            PreparedStatement statement = connect.prepareStatement("SELECT callNum, carName, ccName, company, copyCount FROM car, carCategory WHERE callNum = ? AND car.ccId = carCategory.ccId ORDER BY callNum");
+            statement.setString(1, CallNum);
+            ResultSet result = statement.executeQuery();
 
+            System.out.println("|Call Num|Name|Car Category|Company|Available No. of Copy|");
+            if(result.next()){
+                String callNum = result.getString(1);
+                String carName = result.getString(2);
+                String ccName = result.getString(3);
+                String company = result.getString(4);
+                int copyCount = result.getInt(5);
+                System.out.println("|" + callNum + "|" + carName + "|" + ccName + "|" + company + "|" + copyCount + "|");
+            }else{
+                System.out.println("No matching is found\n");
+            }
+        }catch(SQLException e){
+            System.out.println("[Error] Cannot execute the query.\n");
+        }
+    }
+
+
+    public void listCarByCarName(String CarName){
+        try{
+            PreparedStatement statement = connect.prepareStatement("SELECT callNum, carName, ccName, company, copyCount FROM car, carCategory WHERE carName = ? AND car.ccId = carCategory.ccId ORDER BY callNum");
+            statement.setString(1, CarName);
+            ResultSet result = statement.executeQuery();
+
+            System.out.println("|Call Num|Name|Car Category|Company|Available No. of Copy|");
+            while(result.next()){
+                String callNum = result.getString(1);
+                String carName = result.getString(2);
+                String ccName = result.getString(3);
+                String company = result.getString(4);
+                int copyCount = result.getInt(5);
+                System.out.println("|" + callNum + "|" + carName + "|" + ccName + "|" + company + "|" + copyCount + "|");
+            }
+            System.out.println("End of Query\n");
+        }catch(SQLException e){
+            System.out.println("[Error] Cannot execute the query.\n");
+        }
+    }
+
+    public void listCarByCompany(String Company){
+        try{
+            PreparedStatement statement = connect.prepareStatement("SELECT callNum, carName, ccName, company, copyCount FROM car, carCategory WHERE company = ? AND car.ccId = carCategory.ccId ORDER BY callNum");
+            statement.setString(1, Company);
+            ResultSet result = statement.executeQuery();
+
+            System.out.println("|Call Num|Name|Car Category|Company|Available No. of Copy|");
+            while(result.next()){
+                String callNum = result.getString(1);
+                String carName = result.getString(2);
+                String ccName = result.getString(3);
+                String company = result.getString(4);
+                int copyCount = result.getInt(5);
+                System.out.println("|" + callNum + "|" + carName + "|" + ccName + "|" + company + "|" + copyCount + "|");
+            }
+            System.out.println("End of Query\n");
+        }catch(SQLException e){
+            System.out.println("[Error] Cannot execute the query.\n");
+        }
+    }
+
+    public void listRentRecordByUID(String UID){
+        try{
+            PreparedStatement statement = connect.prepareStatement("SELECT callNum, copyNum, carName, company, checkout, return_date FROM rent, car WHERE rent.uId = ? AND rent.callNum = car.callNum ORDER BY checkout DESC");
+            statement.setString(1, UID);
+            ResultSet result = statement.executeQuery();
+
+            System.out.println("|Call Num|CopyNum|Name|Company|Check-out|Returned?|");
+            while(result.next()){
+                String callNum = result.getString(1);
+                int copyNum = result.getInt(2);
+                String carName = result.getString(3);
+                String company = result.getString(4);
+                String checkout = String.format("%1$tY-%1$tm-%1$td", result.getDate(5));
+                String return_flag;
+                if(result.getDate(6) != null){
+                    return_flag = "Yes";
+                }else{
+                    return_flag = "No";
+                }
+                System.out.println("|" + callNum + "|" + copyNum + "|" + carName + "|" + company + "|" + checkout + "|" + return_flag + "|");
+            }
+            System.out.println("End of Query\n");
+        }catch(SQLException e){
+            System.out.println("[Error] Cannot execute the query.\n");
+        }
+    }
+    
+    //
+    
     public void showSchema(String[] argument) {
 
     }
@@ -113,7 +209,7 @@ public class Database {
 
     }
 
-    
+    //User operation end
 
 
 
