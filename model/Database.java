243ff66ab7ac4d02c805e00bcabdabef363450ca
LamTszHoi <100935287+LamTszHoi@users.noreply.github.com>
2022-04-16 21:37:59 +0800

    Update Database.java

--- model/Database.java
+++ model/Database.java
@@ -190,6 +190,8 @@ public class Database {
         }
     }
     
+    //User operation end
+    
     //
     
     public void showSchema(String[] argument) {
@@ -209,7 +211,7 @@ public class Database {
 
     }
 
-    //User operation end
+    //
 
 
 
